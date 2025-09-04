01d4a9d0ca5dd67583e1aafb8292b4f62f088844
alkofahijo <alkofahijo95@gmail.com>
2025-09-04 12:33:47 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,25 @@
+// db.js
 const { Pool } = require('pg');
+const dns = require('dns');
+
+// Override DNS lookup to force IPv4
+const lookup = (hostname, options, callback) => {
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  options.family = 4; // force IPv4
+  return dns.lookup(hostname, options, callback);
+};
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // store your URI in .env
-  ssl: { rejectUnauthorized: false } // Supabase requires this
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  // override default lookup to force IPv4
+  // pg uses 'lookup' internally
+  // note: 'node-postgres' uses pg.defaults for global settings
+  // since v8, you can pass 'lookup' in Pool config
+  lookup
 });
 
 module.exports = pool;
