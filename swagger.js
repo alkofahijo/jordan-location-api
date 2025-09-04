99581b94724e0aff1f236721b01d9b4e563698c7
alkofahijo <alkofahijo95@gmail.com>
2025-09-04 12:54:06 +0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,11 +1,17 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const fs = require('fs');
+const path = require('path');
 
-swaggerDocument.servers = [
-  { url: process.env.BASE_URL || 'http://localhost:3000/api/v1' }
-];
+// Load your Swagger JSON
+let swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
+);
+
+// Dynamically set server URL based on environment
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1';
+swaggerDocument.servers = [{ url: BASE_URL }];
 
 module.exports = {
   swaggerUi,
-  specs: swaggerDocument
+  specs: swaggerDocument,
 };
