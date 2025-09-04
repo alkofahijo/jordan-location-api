9ea185580af4f33fdcb449b73a9ade8bf4013f43
alkofahijo <alkofahijo95@gmail.com>
2025-09-04 12:10:11 +0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,13 +1,11 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json'); // your JSON file
+const swaggerDocument = require('./swagger.json');
 
-// Dynamically set the server URL based on environment
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1';
 swaggerDocument.servers = [
-  { url: BASE_URL }
+  { url: process.env.BASE_URL || 'http://localhost:3000/api/v1' }
 ];
 
 module.exports = {
   swaggerUi,
-  specs: swaggerDocument,
+  specs: swaggerDocument
 };
