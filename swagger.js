b1a48c8ed29fbcbeb07f924ed7da9d4c84a41863
alkofahijo <alkofahijo95@gmail.com>
2025-09-06 02:16:01 +0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,21 +1,8 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+// swagger.js
 const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); // path to your full swagger.json
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Jordan Locations API',
-      version: '1.0.0',
-      description: 'Read-only API for governorates, districts, and areas in Jordan',
-    },
-    servers: [
-      { url: process.env.BASE_URL || 'http://localhost:10000' },
-    ],
-  },
-  apis: ['./routes/*.js'], // Load swagger comments from route files
+module.exports = {
+  swaggerUi,
+  specs: swaggerDocument
 };
-
-const specs = swaggerJsdoc(options);
-
-module.exports = { swaggerUi, specs };
