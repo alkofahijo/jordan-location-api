465542b5bc88d1d1a6e940d16dc40ac4e3661e4d
alkofahijo <alkofahijo95@gmail.com>
2025-09-06 01:21:31 +0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,17 +1,29 @@
+// swagger.js
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
+const swaggerJsdoc = require('swagger-jsdoc');
 
-// Load your Swagger JSON
-let swaggerDocument = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
-);
-
-// Dynamically set server URL based on environment
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1';
-swaggerDocument.servers = [{ url: BASE_URL }];
-
-module.exports = {
-  swaggerUi,
-  specs: swaggerDocument,
+// Swagger definition
+const options = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'Jordan Locations API',
+      version: '1.0.0',
+      description: 'API for all governorates, districts, and areas in Jordan.',
+      contact: {
+        name: 'FutureTech Volunteer Team',
+        url: 'https://jordan-location-api.onrender.com',
+      },
+    },
+    servers: [
+      {
+        url: 'https://jordan-location-api.onrender.com/api/v1',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Points to routes for JSDoc comments
 };
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
